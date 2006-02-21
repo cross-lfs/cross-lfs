4e71b7e833cd38600fcad66bb2e3f7bf9ec32358
Jim Gifford <clfs@jg555.com>
2006-02-21 08:22:44 +0000

    r599@server (orig r597):  ryan | 2005-08-05 23:16:50 -0700  General fixes, add pgsql support

--- scripts/untested/blfs-scripts/blfs-samba.sh
+++ scripts/untested/blfs-scripts/blfs-samba.sh
@@ -10,22 +10,19 @@ set_buildenv
 set_libdirname
 setup_multiarch
 if [ ! "${libdirname}" = "lib" ]; then
-   extra_conf="--libdir=/usr/${libdirname}"
+   extra_conf="--libdir=/usr/${libdirname}/samba"
 fi
 
-unpack_tarball samba-${MYSQL_VER}
+unpack_tarball samba-${SAMBA_VER}
 
-# TODO, set --with-libdir here only when biarch...
-#       remove from configure below
 cd ${PKGDIR}/source
 
-max_log_init samba ${MYSQL_VER} "blfs (shared)" ${CONFLOGS} ${LOG}
+max_log_init samba ${SAMBA_VER} "blfs (shared)" ${CONFLOGS} ${LOG}
 CC="${CC-gcc} ${ARCH_CFLAGS}" \
 CXX="${CXX-g++} ${ARCH_CFLAGS}" \
-CFLAGS="${TGT_CFLAGS}" \
-CXXLAGS="${TGT_CFLAGS}" \
+CFLAGS="-O2 -pipe ${TGT_CFLAGS}" \
+CXXLAGS="-O2 -pipe ${TGT_CFLAGS}" \
 ./configure --prefix=/usr ${extra_conf} \
-   --with-libdir=/usr/${libdirname}/samba \
    --sysconfdir=/etc \
    --with-configdir=/etc/samba \
    --localstatedir=/var \
@@ -33,19 +30,20 @@ CXXLAGS="${TGT_CFLAGS}" \
    --with-fhs \
    --with-smbmount \
    --with-automount \
-   --with-python=python${suffix} \
+   --with-python \
    --with-pam \
-   --with-expsam=xml,mysql \
+   --with-expsam=xml,mysql,pgsql \
    >> ${LOGFILE} 2>&1 &&
 echo " o Configure OK" &&
 
+# BAH, samba doesn't seem to want to honour --libdir
 min_log_init ${BUILDLOGS} &&
-make ${PMFLAGS} \
+make LIBDIR=/usr/${libdirname}/samba \
    >> ${LOGFILE} 2>&1 &&
 echo " o Build OK" &&
 
 min_log_init ${INSTLOGS} &&
-make install \
+make LIBDIR=/usr/${libdirname}/samba install \
    >> ${LOGFILE} 2>&1 &&
 echo " o ALL OK" || barf
 
@@ -57,7 +55,7 @@ chmod 644 /usr/include/libsmbclient.h &&
 install -m755 nsswitch/libnss_win{s,bind}.so /${libdirname} &&
 ln -sf libnss_winbind.so /${libdirname}/libnss_winbind.so.2 &&
 ln -sf libnss_wins.so /${libdirname}/libnss_wins.so.2 &&
-cp ../examples/smb.conf.default /etc/samba &&
+cp ../examples/smb.conf.default /tmp/${PKGDIR}-${BUILDENV}/etc/samba &&
 install -m644 ../docs/*.pdf /usr/share/samba &&
 if [ -f nsswitch/pam_winbind.so ]; then 
     install -m755 nsswitch/pam_winbind.so /${libdirname}/security
