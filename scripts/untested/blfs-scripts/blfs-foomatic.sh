83ea618ac59e0726bbd9a2d8bdac57d340800b46
Jim Gifford <clfs@jg555.com>
2006-02-21 08:25:06 +0000

    r613@server (orig r611):  ryan | 2005-08-05 23:41:20 -0700  Update

--- scripts/untested/blfs-scripts/blfs-foomatic.sh
+++ scripts/untested/blfs-scripts/blfs-foomatic.sh
@@ -18,14 +18,14 @@ CC="${CC-gcc} ${ARCH_CFLAGS}" \
 echo " o Configure OK" &&
 
 min_log_init ${INSTLOGS}
-make install 
+make install \
   >> ${LOGFILE} 2>&1 &&
 echo " o Install OK" || barf
 
 cd ${SRC}
 LOG=foomatic-db-hpijs-blfs.log
 
-unpack_tarball foomatic-db-hpijs-${FOOMATIC_DB_HPIJS_VER}
+unpack_tarball foomatic-db-hpijs-${FOOMATIC_DB_HPIJS_VER}-current
 cd ${PKGDIR}
 
 max_log_init foomatic-db ${FOOMATIC_DB_HPIJS_VER} "blfs (shared)" ${CONFLOGS} ${LOG}
@@ -34,8 +34,13 @@ CC="${CC-gcc} ${ARCH_CFLAGS}" \
   >> ${LOGFILE} 2>&1 &&
 echo " o Configure OK" &&
 
+min_log_init ${BUILDLOGS}
+make \
+  >> ${LOGFILE} 2>&1 &&
+echo " o Build OK" || barf
+
 min_log_init ${INSTLOGS}
-make install 
+make install \
   >> ${LOGFILE} 2>&1 &&
 echo " o Install OK" || barf
 
@@ -51,38 +56,13 @@ CC="${CC-gcc} ${ARCH_CFLAGS}" \
   >> ${LOGFILE} 2>&1 &&
 echo " o Configure OK" &&
 
-min_log_init ${INSTLOGS}
-make install
+min_log_init ${BUILDLOGS}
+make \
   >> ${LOGFILE} 2>&1 &&
-echo " o Install OK" || barf
-
-if [ ! "${libdirname}" = "lib" ]; then
-   # additional symlinks for lib64 cups, ppr to get the foomatic filters
-   # TODO: this needs to be done better
-
-   mkdir -p /usr/${libdirname}/cups/filter
-   ln -sf /usr/bin/foomatic-rip /usr/${libdirname}/cups/filter
-   mkdir -p /usr/${libdirname}/ppr/interfaces
-   mkdir -p /usr/${libdirname}/ppr/lib
-   ln -sf /usr/bin/foomatic-rip /usr/${libdirname}/ppr/interfaces
-   ln -sf /usr/bin/foomatic-rip /usr/${libdirname}/ppr/lib
-fi
-
-#### we probably want libxml2 
-cd ${SRC}
-LOG=foomatic-db-engine-blfs.log
-
-unpack_tarball foomatic-db-engine-${FOOMATIC_DB_ENG_VER}
-cd ${PKGDIR}
-
-max_log_init foomatic-db-engine ${FOOMATIC_DB_ENG_VER} "blfs (shared)" ${CONFLOGS} ${LOG}
-CC="${CC-gcc} ${ARCH_CFLAGS}" \
-./configure --prefix=/usr \
-  >> ${LOGFILE} 2>&1 &&
-echo " o Configure OK" &&
+echo " o Build OK" || barf
 
 min_log_init ${INSTLOGS}
-make install
+make install \
   >> ${LOGFILE} 2>&1 &&
 echo " o Install OK" || barf
 
