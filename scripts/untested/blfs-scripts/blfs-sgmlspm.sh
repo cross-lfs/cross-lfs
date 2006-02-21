8a3554dc00f9844fdcd2918afc0b8d22cd8c8673
Jim Gifford <clfs@jg555.com>
2006-02-21 08:24:48 +0000

    r611@server (orig r609):  ryan | 2005-08-05 23:37:09 -0700  Ensure all output goes to the logfiles

--- scripts/untested/blfs-scripts/blfs-sgmlspm.sh
+++ scripts/untested/blfs-scripts/blfs-sgmlspm.sh
@@ -14,19 +14,20 @@ unpack_tarball SGMLSpm-${SGMLSPM_VER}
 cd ${PKGDIR}
 
 max_log_init sgmlspm ${SGMLSPM_VER} "blfs (shared)" ${CONFLOGS} ${LOG}
+(
 sed -i -e "s@/usr/local/bin@/usr/bin@" \
        -e "s@/usr/local/lib/perl5@/usr/${libdirname}/perl5/site_perl/${PERL_VER}@" \
        -e "s@/usr/local/lib/www/docs@/usr/share/doc/perl5@" \
-   Makefile \
-   >> ${LOGFILE} 2>&1 &&
+   Makefile
+) >> ${LOGFILE} 2>&1 &&
+echo " o Configure OK" || barf
 
 min_log_init ${INSTLOGS} &&
-make install \
-   >> ${LOGFILE} 2>&1 &&
-echo " o ALL OK" || barf
-
+(
+make install &&
 install -d -m 755 /usr/share/doc/perl5 &&
 make install_html &&
 rm -f /usr/share/doc/perl5/SGMLSpm/sample.pl &&
 install -m 644 DOC/sample.pl /usr/share/doc/perl5/SGMLSpm
-
+) >> ${LOGFILE} 2>&1 &&
+echo " o ALL OK" || barf
