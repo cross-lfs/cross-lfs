06fb515791830e689ae7b7639f4f757f27637c62
Jim Gifford <clfs@jg555.com>
2009-02-07 20:04:51 +0000

    Updated Linux to 2.6.28.4

--- scripts/patch/binutils-patch.sh
+++ scripts/patch/binutils-patch.sh
@@ -4,27 +4,29 @@
 # Get Version #
 #
 VERSION=$1
+SOURCEVERSION=$2
 
 # Check Input
 #
 if [ "${VERSION}" = "" ]; then
   echo "$0 - Binutils_Version"
-  echo "This will Create a Patch for Binutils Binutils_Version"
+  echo "This will Create a Patch for Binutils Binutils_Series Binutils_Version"
   exit 255
 fi
 
+# 
 # Download Binutils Source
 #
 cd /usr/src
-if ! [ -e binutils-${VERSION}.tar.bz2  ]; then
-  wget ftp://ftp.gnu.org/gnu/binutils/binutils-${VERSION}.tar.bz2
+if ! [ -e binutils-${SOURCEVERSION}.tar.bz2  ]; then
+  wget ftp://ftp.gnu.org/gnu/binutils/binutils-${SOURCEVERSION}.tar.bz2
 fi
 
 # Cleanup Directory
 #
-rm -rf binutils-${VERSION} binutils-${VERSION}.orig
-tar xvf binutils-${VERSION}.tar.bz2
-mv binutils-${VERSION} binutils-${VERSION}.orig
+rm -rf binutils-${SOURCEVERSION} binutils-${SOURCEVERSION}.orig
+tar xvf binutils-${SOURCEVERSION}.tar.bz2
+mv binutils-${SOURCEVERSION} binutils-${SOURCEVERSION}.orig
 CURRENTDIR=$(pwd -P)
 
 # Get Current Updates from CVS
@@ -32,11 +34,11 @@ CURRENTDIR=$(pwd -P)
 cd /usr/src
 FIXEDVERSION=$(echo ${VERSION} | sed -e 's/\./_/g')
 cvs -z 9 -d :pserver:anoncvs@sourceware.org:/cvs/src export -rbinutils-${FIXEDVERSION}-branch binutils
-mv src binutils-${VERSION}
+mv src binutils-${SOURCEVERSION}
 
 # Cleanup
 #
-DIRS="binutils-${VERSION} binutils-${VERSION}.orig"
+DIRS="binutils-${SOURCEVERSION} binutils-${SOURCEVERSION}.orig"
 for DIRECTORY in ${DIRS}; do
   cd /usr/src/${DIRECTORY}
   FILE_LIST=".cvsignore *.gmo"
@@ -54,25 +56,25 @@ for DIRECTORY in ${DIRS}; do
     done
     cd ..
 done
-cd /usr/src/binutils-${VERSION}
-rm -f /usr/src/binutils-${VERSION}.orig/md5.sum
+cd /usr/src/binutils-${SOURCEVERSION}
+rm -f /usr/src/binutils-${SOURCEVERSION}.orig/md5.sum
 
 # Make Binutils a Release
 #
-cd /usr/src/binutils-${VERSION}
+cd /usr/src/binutils-${SOURCEVERSION}
 sed -i 's/# RELEASE=y/RELEASE=y/g' bfd/Makefile.am
 sed -i 's/# RELEASE=y/RELEASE=y/g' bfd/Makefile.in
 
 # Create Patch
 #
 cd /usr/src
-echo "Submitted By: Jim Gifford (jim at cross-lfs dot org)" > binutils-${VERSION}-branch_update-x.patch
-echo "Date: `date +%m-%d-%Y`" >> binutils-${VERSION}-branch_update-x.patch
-echo "Initial Package Version: ${VERSION}" >> binutils-${VERSION}-branch_update-x.patch
-echo "Origin: Upstream" >> binutils-${VERSION}-branch_update-x.patch
-echo "Upstream Status: Applied" >> binutils-${VERSION}-branch_update-x.patch
-echo "Description: This is a branch update for binutils-${VERSION}, and should be" >> binutils-${VERSION}-branch_update-x.patch
-echo "             rechecked periodically." >> binutils-${VERSION}-branch_update-x.patch
-echo "" >> binutils-${VERSION}-branch_update-x.patch
-diff -Naur binutils-${VERSION}.orig binutils-${VERSION} >> binutils-${VERSION}-branch_update-x.patch
-echo "Created /usr/src/binutils-${VERSION}-branch_update-x.patch."
+echo "Submitted By: Jim Gifford (jim at cross-lfs dot org)" > binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Date: `date +%m-%d-%Y`" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Initial Package Version: ${SOURCEVERSION}" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Origin: Upstream" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Upstream Status: Applied" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Description: This is a branch update for binutils-${SOURCEVERSION}, and should be" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "             rechecked periodically." >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "" >> binutils-${SOURCEVERSION}-branch_update-x.patch
+diff -Naur binutils-${SOURCEVERSION}.orig binutils-${SOURCEVERSION} >> binutils-${SOURCEVERSION}-branch_update-x.patch
+echo "Created /usr/src/binutils-${SOURCEVERSION}-branch_update-x.patch."
